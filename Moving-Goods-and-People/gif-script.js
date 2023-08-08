49d9261716a04f2e7c04e932d589cd35cb39e15c
jevalentini@gmail.com <jevalentini@gmail.com>
2023-08-08 11:04:23 -0300

    Primera prueba de correccion

--- Moving-Goods-and-People/gif-script.js
+++ Moving-Goods-and-People/gif-script.js
@@ -1,14 +1,14 @@
-function loadGIF(){
-	var parentElement_prob = document.getElementById("prob_gif_container");
-	const imgElement_prob = document.createElement("img");
-	imgElement_prob.src = "transportation_prob.gif"; 
-	imgElement_prob.id = "prob_gif_image";
-	parentElement_prob.appendChild(imgElement_prob);
+function loadGIF() {
+    var parentElement_prob = document.getElementById("prob_gif_container");
+    var imgElement_prob = document.createElement("img");
+    imgElement_prob.src = "transportation_prob.gif"; 
+    imgElement_prob.id = "prob_gif_image";
+    parentElement_prob.appendChild(imgElement_prob);
 
-	var parentElement_sol = document.getElementById("sol_gif_container");
-	const imgElement_sol = document.createElement("img");
-	imgElement_sol.src = "transportation.gif"; 
-	imgElement_sol.id = "sol_gif_image";
-	parentElement_sol.appendChild(imgElement_sol);
+    var parentElement_sol = document.getElementById("sol_gif_container");
+    var imgElement_sol = document.createElement("img");
+    imgElement_sol.src = "transportation.gif"; 
+    imgElement_sol.id = "sol_gif_image";
+    parentElement_sol.appendChild(imgElement_sol);
 }
-export { loadGIF };
+export{ loadGIF };
